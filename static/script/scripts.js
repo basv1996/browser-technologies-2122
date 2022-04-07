c66dfcd90559596600992900b1f23323d49c4370
basv1996 <bas.vugts@gmail.com>
2022-04-07 14:38:49 +0200

    client side JS

---
+++ static/script/scripts.js
@@ -0,0 +1,12 @@
+var shirtjeSVG = document.querySelector("#shirtje")
+var color = document.querySelector(".homePageForm select#color")
+var textInput = document.querySelector("#textInput")
+var textOnShirt = document.querySelector(".textOpShirtje")
+
+color.addEventListener("change", function() {
+    shirtjeSVG.style.fill=color.value
+})
+
+textInput.addEventListener("keyup", function() {
+    textOnShirt.innerHTML = textInput.value
+})
